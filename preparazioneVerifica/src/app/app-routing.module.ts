b9475bca3515e51d5c68b6fd8bbbe7095b1ec801
Santambrogio-A <santambrogio.andrea@iisgalvanimi.edu.it>
2022-10-11 11:31:13 +0000

    Punto 3

--- preparazioneVerifica/src/app/app-routing.module.ts
+++ preparazioneVerifica/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   { path: 'search', component: SearchComponent },
-  //{ path: 'item/:id', component: ItemComponent },
+  { path: 'product/:id', component: ProductComponent },
 ];
 
 @NgModule({
--- preparazioneVerifica/src/app/app.module.ts
+++ preparazioneVerifica/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
