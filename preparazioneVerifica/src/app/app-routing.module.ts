747184a41b8cc2af19746affff2c5ae5db90e271
Santambrogio-A <santambrogio.andrea@iisgalvanimi.edu.it>
2022-10-11 11:31:40 +0000

    Punto 3

--- preparazioneVerifica/src/app/app-routing.module.ts
+++ preparazioneVerifica/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
 import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
